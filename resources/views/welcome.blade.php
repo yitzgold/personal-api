c2cb682c9db2ab6b1c11d10df4c5e930d2271d07
yitzi goldman <yitzigoldman@gmail.com>
2018-05-31 11:01:48 -0400

    test2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,7 +65,7 @@
         </style>
     </head>
     <body>
-    <h1>test<h1>
+    <h1>test2<h1>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">

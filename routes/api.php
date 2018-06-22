84ec4d7b71c07480bf56763d3a17e612fd968218
yitzi goldman <yitzigoldman@gmail.com>
2018-06-22 11:36:06 -0400

    added docs

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/', function () {
+    return view('docs');
+});
+
 Route::get('/institutions', 'InstitutionsController@all');
 
 Route::get('/institutions/{id}', 'InstitutionsController@oneById');
